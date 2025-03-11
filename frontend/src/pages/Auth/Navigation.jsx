284f90b52bc876c268782e3c22a4e1f1405674d1
saurabhKrOO7 <saurabhkr44002211@gmail.com>
2025-03-11 16:11:40 +0530

    logo fixing

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -63,8 +63,7 @@ const Navigation = () => {
       <nav className="nav container">
         <div className="nav__data">
           <Link to="/" className="nav__logo">
-            <MdClose className="ri-planet-line" />
-            RentEase
+            <img src="/images/logo.png" alt="Logo" />
           </Link>
 
           <div className="nav__toggle" id="nav-toggle">
